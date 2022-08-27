5d792ce9d03aeda49d95998d65572fb29085bb2f
Dowon Kang (강도원) <91768420+onekorea37@users.noreply.github.com>
2022-08-27 12:20:22 -0400

    #10718

--- Main.java
+++ Main.java
@@ -1,7 +1,7 @@
 
 class Main {
-		public static void main(String[] args) {
-			
-			System.out.print("Hello World!");
-		}
+	public static void main(String[] args) {
+		
+		System.out.print("강한친구 대한육군\n강한친구 대한육군");
+	}
 }
